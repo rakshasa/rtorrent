dfa6c9cd05e3ca9670eb467a69cdc0ad77bfd88b
rakshasa <sundell.software@gmail.com>
2005-03-14 20:37:57 +0000

    Added our id to the peer info screen.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@354 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_peer_info.cc
+++ src/display/window_peer_info.cc
@@ -48,6 +48,7 @@ WindowPeerInfo::redraw() {
   torrent::Download d = m_download->get_download();
 
   m_canvas->print(0, y++, "Hash: %s", utils::string_to_hex(d.get_hash()).c_str());
+  m_canvas->print(0, y++, "Id:   %s", utils::escape_string(d.get_id()).c_str());
   m_canvas->print(0, y++, "Chunks: %u / %u * %u",
 		  d.get_chunks_done(),
 		  d.get_chunks_total(),
@@ -60,6 +61,8 @@ WindowPeerInfo::redraw() {
 
     return;
   }
+
+  m_canvas->print(0, y++, "*** Peer Info ***");
 
   m_canvas->print(0, y++, "DNS: %s:%hu", (*m_focus)->get_dns().c_str(), (*m_focus)->get_port());
   m_canvas->print(0, y++, "Id: %s" , utils::escape_string((*m_focus)->get_id()).c_str());
