98676e5531b038e0c624bce5bc12ea52a8783a9d
rakshasa <sundell.software@gmail.com>
2005-04-28 01:15:39 +0000

    Removed choke delay in the peer list window.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@421 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_peer_list.cc
+++ src/display/window_peer_list.cc
@@ -91,12 +91,11 @@ WindowPeerList::redraw() {
     m_canvas->print(x, y, "%.1f", (double)p.get_rate_peer() / 1024);
     x += 7;
 
-    m_canvas->print(x, y, "%c%c/%c%c%c",
+    m_canvas->print(x, y, "%c%c/%c%c",
 		    p.get_remote_choked() ? 'c' : 'u',
 		    p.get_remote_interested() ? 'i' : 'n',
 		    p.get_local_choked() ? 'c' : 'u',
-		    p.get_local_interested() ? 'i' : 'n',
-		    p.get_choke_delayed() ? 'd' : ' ');
+		    p.get_local_interested() ? 'i' : 'n');
     x += 7;
 
     m_canvas->print(x, y, "%i/%i",
