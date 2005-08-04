dc5476e3f46538f36b74c12473ff0e07893cc1d7
rakshasa <sundell.software@gmail.com>
2005-08-04 15:41:15 +0000

    * Moved Hanshake* to protocol/, moved peer/* to protocol/*.
    * Show in client which tracker has the focus.
    
    
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@516 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_tracker_list.cc
+++ src/display/window_tracker_list.cc
@@ -84,12 +84,13 @@ WindowTrackerList::redraw() {
 		    range.first == *m_focus ? '*' : ' ',
 		    t.get_url().c_str());
 
-    m_canvas->print(0, pos++, "%c Group: %2i Id: %s Enabled: %s Open: %s",
+    m_canvas->print(0, pos++, "%c Group: %2i Id: %s Focus: %s Enabled: %s Open: %s",
 		    range.first == *m_focus ? '*' : ' ',
 		    t.get_group(),
 		    utils::escape_string(t.get_tracker_id()).c_str(),
-		    t.is_enabled() ? "yes" : "no",
-		    t.is_open() ? "yes" : "no");
+		    range.first == m_download->get_download().get_tracker_focus() ? "yes" : " no",
+		    t.is_enabled() ? "yes" : " no",
+		    t.is_open() ? "yes" : " no");
 
     ++range.first;
   }
