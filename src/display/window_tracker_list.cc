c77ecb034f4a446a3202d04e78edc0ad4954843c
rakshasa <sundell.software@gmail.com>
2006-02-28 13:20:52 +0000

    * Added API for getting the complete, incomplete and downloaded stats received from trackers. Display them in the tracker list.
    * Fixed a bug introduced by the TrackerInfo* id change that caused
    handshakes to hold old pointer. They now get removed when a download
    is erased.
    
    * Some cleanup of DownloadWrapper initialization.
    
    * Moved bind and local address to from Manager to SocketManager.
    
    
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@645 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_tracker_list.cc
+++ src/display/window_tracker_list.cc
@@ -84,13 +84,15 @@ WindowTrackerList::redraw() {
 		    range.first == *m_focus ? '*' : ' ',
 		    t.url().c_str());
 
-    m_canvas->print(0, pos++, "%c Group: %2i Id: %s Focus: %s Enabled: %s Open: %s",
+    m_canvas->print(0, pos++, "%c Group: %2i Id: %s Focus: %s Enabled: %s Open: %s S/L: %u/%u",
 		    range.first == *m_focus ? '*' : ' ',
 		    t.group(),
 		    rak::copy_escape_html(t.tracker_id()).c_str(),
 		    range.first == m_download->get_download().tracker_focus() ? "yes" : " no",
 		    t.is_enabled() ? "yes" : " no",
-		    t.is_open() ? "yes" : " no");
+		    t.is_open() ? "yes" : " no",
+		    t.scrape_complete(),
+		    t.scrape_incomplete());
 
     ++range.first;
   }
