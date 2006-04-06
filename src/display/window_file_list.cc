7ad18cdac2e6248e25f2caf7ddd8306df7a5e3c1
rakshasa <sundell.software@gmail.com>
2006-04-06 17:51:17 +0000

    * Added missing files in rtorrent/Makefile.am.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@666 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_file_list.cc
+++ src/display/window_file_list.cc
@@ -135,9 +135,11 @@ WindowFileList::redraw() {
 		    done_percentage(e),
 		    e.path()->encoding().c_str());
 
-    m_canvas->print(84, pos, "%i - %i",
+    m_canvas->print(84, pos, "%i - %i %c%c",
 		    e.chunk_begin(),
-		    e.chunk_begin() != e.chunk_end() ? (e.chunk_end() - 1) : e.chunk_end());
+		    e.chunk_begin() != e.chunk_end() ? (e.chunk_end() - 1) : e.chunk_end(),
+		    e.is_created() ? 'E' : 'M',
+		    e.is_correct_size() ? 'C' : 'W');
 
     ++range.first;
     ++pos;
