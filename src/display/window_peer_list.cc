44a782e9624b3348ac0eedc9513f5ebf5af50c74
rakshasa <sundell.software@gmail.com>
2006-10-18 07:25:06 +0000

    * Cleanp of the encryption patch, minor changes.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@793 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_peer_list.cc
+++ src/display/window_peer_list.cc
@@ -107,8 +107,7 @@ WindowPeerList::redraw() {
     m_canvas->print(x, y, "%.1f", (double)p.peer_rate()->rate() / 1024); x += 7;
 
     m_canvas->print(x, y, "%c/%c%c/%c%c",
-                    p.is_encrypted() ? p.is_incoming() ? 'R' : 'L' :
-                    p.is_incoming() ? 'r' : 'l',
+                    p.is_encrypted() ? (p.is_incoming() ? 'R' : 'L') : (p.is_incoming() ? 'r' : 'l'),
                     p.is_remote_choked() ? 'c' : 'u',
                     p.is_remote_interested() ? 'i' : 'n',
                     p.is_local_choked() ? 'c' : 'u',
