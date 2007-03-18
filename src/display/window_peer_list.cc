3b13fc7f05eb4518e91f659db0b134280b807766
rakshasa <sundell.software@gmail.com>
2007-03-18 01:56:29 +0000

    * Send uninterested to peer when no download slots are available, and various other stuff to make it work properly.
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@870 e378c898-3ddf-0310-93e7-cc216c733640

--- src/display/window_peer_list.cc
+++ src/display/window_peer_list.cc
@@ -108,9 +108,19 @@ WindowPeerList::redraw() {
     m_canvas->print(x, y, "%.1f", (double)p.down_rate()->rate() / 1024); x += 7;
     m_canvas->print(x, y, "%.1f", (double)p.peer_rate()->rate() / 1024); x += 7;
 
+    char remoteChoked;
+
+    if (!p.is_remote_choked_limited())
+      remoteChoked = 'U';
+    else if (p.is_remote_queued())
+      remoteChoked = 'Q';
+    else
+      remoteChoked = 'C';
+
     m_canvas->print(x, y, "%c/%c%c/%c%c",
                     p.is_encrypted() ? (p.is_incoming() ? 'R' : 'L') : (p.is_incoming() ? 'r' : 'l'),
-                    p.is_remote_queued() ? (p.is_remote_choked() ? 'C' : 'u') : (p.is_remote_choked() ? 'c' : 'U'),
+                    p.is_remote_choked() ? std::tolower(remoteChoked) : remoteChoked,
+
                     p.is_remote_interested() ? 'i' : 'n',
                     p.is_local_choked() ? 'c' : 'u',
                     p.is_local_interested() ? 'i' : 'n');
