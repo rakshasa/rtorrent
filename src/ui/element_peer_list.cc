1b96bd6016212069946e350b061dbf6ea3c88e67
rakshasa <sundell.software@gmail.com>
2008-01-28 18:21:36 +0000

    * Bumped to version 0.12.0/0.8.0.
    * Removed Download::disconnect_peer as it can call ConnectionList
    directly.
    
    
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@1035 e378c898-3ddf-0310-93e7-cc216c733640

--- src/ui/element_peer_list.cc
+++ src/ui/element_peer_list.cc
@@ -211,7 +211,7 @@ ElementPeerList::receive_disconnect_peer() {
   if (m_listItr == m_list.end())
     return;
 
-  m_download->download()->disconnect_peer(*m_listItr);
+  m_download->connection_list()->erase(*m_listItr, 0);
 }
 
 void
