df4c0ce67bd9bd2e993746dcc4fe5e93f3552453
rakshasa <sundell.software@gmail.com>
2010-03-04 17:35:46 +0000

    * Optimized bencode read skip function.
    * Fixed various bugs in the new dht/static map implementation.
    
    
    git-svn-id: svn://rakshasa.no/libtorrent/trunk/rtorrent@1140 e378c898-3ddf-0310-93e7-cc216c733640

--- src/core/download_factory.cc
+++ src/core/download_factory.cc
@@ -314,9 +314,6 @@ DownloadFactory::receive_success() {
 
 void
 DownloadFactory::receive_failed(const std::string& msg) {
-  if (m_stream == NULL)
-    throw torrent::internal_error("DownloadFactory::receive_success() called on an object with m_stream == NULL");
-
   // Add message to log.
   if (m_printLog) {
     m_manager->get_log_important().push_front(msg + ": \"" + m_uri + "\"");
