d8aa1cd28bc0b7dd96120eaa7a16422983be497c
Jari Sundell <sundell.software@gmail.com>
2011-12-01 02:26:48 +0900

    Display promiscuous and failure mode in tracker display.

--- src/display/window_tracker_list.cc
+++ src/display/window_tracker_list.cc
@@ -65,7 +65,11 @@ WindowTrackerList::redraw() {
   torrent::TrackerList* tl = m_download->tracker_list();
   torrent::TrackerController* tc = m_download->tracker_controller();
 
-  m_canvas->print(2, pos, "Trackers: [Key: %08x] [Requesting: %s]", tl->key(), tc->is_requesting() ? "yes" : "no");
+  m_canvas->print(2, pos, "Trackers: [Key: %08x] [%s %s %s]",
+                  tl->key(),
+                  tc->is_requesting() ? "req" : "   ",
+                  tc->is_promiscuous_mode() ? "prom" : "    ",
+                  tc->is_failure_mode() ? "fail" : "    ");
   ++pos;
 
   if (tl->size() == 0 || *m_focus >= tl->size())
