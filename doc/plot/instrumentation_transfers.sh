dea7c80996ec5a98394a2c418f12ae2e53cb4a48
rakshasa <sundell.software@gmail.com>
2014-04-01 01:03:44 +0200

    Updated instrumenation transfer plot script.

--- doc/plot/instrumentation_transfers.sh
+++ doc/plot/instrumentation_transfers.sh
@@ -14,20 +14,30 @@ set datafile missing '0'
 set format y "%.0f"
 set format y2 "%.0f"
 
+set y2range [0:]
+
 set output "output_$1_transfer_requests.png"
 plot \
-     "instrumentation_transfers.log.$1" using 1:2  title 'downloading'     smooth sbezier with lines lw 4 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:3  title 'finished'        smooth sbezier with lines lw 4 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:4  title 'skipped'         smooth sbezier with lines lw 2 axis x1y2,\
-     "instrumentation_transfers.log.$1" using 1:5  title 'unknown'         smooth sbezier with lines lw 2 axis x1y2
+     "instrumentation_transfers.log.$1" using 1:2  title 'delegated'       smooth sbezier with lines lw 4 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:3  title 'downloading'     smooth sbezier with lines lw 4 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:4  title 'finished'        smooth sbezier with lines lw 4 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:5  title 'skipped'         smooth sbezier with lines lw 2 axis x1y2,\
+     "instrumentation_transfers.log.$1" using 1:6  title 'unknown'         smooth sbezier with lines lw 2 axis x1y2,\
+     "instrumentation_transfers.log.$1" using 1:19 title 'unaccounted'     smooth sbezier with lines lw 2 axis x1y2
 
 set output "output_$1_transfer_queues.png"
 plot \
-     "instrumentation_transfers.log.$1" using 1:6  title 'queue added'      smooth sbezier with lines lw 2 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:7  title 'queue removed'    smooth sbezier with lines lw 2 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:8  title 'queue total'      smooth sbezier with lines lw 4 axis x1y2,\
-     "instrumentation_transfers.log.$1" using 1:9  title 'canceled added'   smooth sbezier with lines lw 2 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:10 title 'canceled removed' smooth sbezier with lines lw 2 axis x1y1,\
-     "instrumentation_transfers.log.$1" using 1:11 title 'canceled total'   smooth sbezier with lines lw 4 axis x1y2
+     "instrumentation_transfers.log.$1" using 1:7  title 'queue added'      smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:8  title 'queue moved'      smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:9  title 'queue removed'    smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:10 title 'queue total'      smooth sbezier with lines lw 4 axis x1y2,\
+     "instrumentation_transfers.log.$1" using 1:11 title 'canceled added'   smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:12 title 'canceled moved'   smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:13 title 'canceled removed' smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:14 title 'canceled total'   smooth sbezier with lines lw 4 axis x1y2,\
+     "instrumentation_transfers.log.$1" using 1:15 title 'choked added'     smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:16 title 'choked moved'     smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:17 title 'choked removed'   smooth sbezier with lines lw 2 axis x1y1,\
+     "instrumentation_transfers.log.$1" using 1:18 title 'choked total'     smooth sbezier with lines lw 4 axis x1y2
 
 EOF
