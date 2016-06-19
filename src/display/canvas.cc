7ea572967600a196a16e6c98768c6b57d7716682
zp <thatguyzp@gmail.com>
2016-06-19 07:04:23 -0500

    trim modified files

--- src/display/canvas.cc
+++ src/display/canvas.cc
@@ -5,12 +5,12 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -104,7 +104,7 @@ void
 Canvas::initialize() {
   if (m_isInitialized)
     return;
-  
+
   m_isDaemon = rpc::call_command_value("system.use_daemon");
 
   m_isInitialized = true;
@@ -123,7 +123,7 @@ void
 Canvas::cleanup() {
   if (!m_isInitialized)
     return;
-  
+
   m_isInitialized = false;
 
   if (!m_isDaemon) {
