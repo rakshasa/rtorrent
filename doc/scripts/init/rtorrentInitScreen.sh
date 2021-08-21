cdb55c5a9f0e38cafe2c250960da378fc74d91a6
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-08-21 15:35:54 +0000

    Unify codestyle in scripts to make comparison easier
    This makes it easier to see what the exact differences are between the
    three files, as well as fixes a mix of tabs and spaces.

--- doc/scripts/init/rtorrentInitScreen.sh
+++ doc/scripts/init/rtorrentInitScreen.sh
@@ -7,7 +7,7 @@
 # explicit session directory using ABSOLUTE paths (no, ~ is not absolute) in your rtorrent.rc.
 # If you typically just start rtorrent with just "rtorrent" on the
 # command line, all you need to change is the "user" option.
-# Attach to the screen session as your user with 
+# Attach to the screen session as your user with
 # "screen -dr rtorrent". Change "rtorrent" with srnname option.
 # Licensed under the GPLv2 by lostnihilist: lostnihilist _at_ gmail _dot_ com
 ##############
@@ -17,18 +17,18 @@
 #######################
 ##Start Configuration##
 #######################
-# You can specify your configuration in a different file 
+# You can specify your configuration in a different file
 # (so that it is saved with upgrades, saved in your home directory,
-# or whateve reason you want to)
+# or whatever reason you want to)
 # by commenting out/deleting the configuration lines and placing them
 # in a text file (say /home/user/.rtorrent.init.conf) exactly as you would
 # have written them here (you can leave the comments if you desire
-# and then uncommenting the following line correcting the path/filename 
+# and then uncommenting the following line correcting the path/filename
 # for the one you used. note the space after the ".".
 # . /etc/rtorrent.init.conf
 
 #Do not put a space on either side of the equal signs e.g.
-# user = user 
+# user = user
 # will not work
 # system user to run as
 user="user"
@@ -53,6 +53,7 @@ logfile="/var/log/rtorrentInit.log"
 #######################
 ###END CONFIGURATION###
 #######################
+
 PATH=/usr/bin:/usr/local/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin
 DESC="rtorrent"
 NAME=rtorrent
@@ -60,26 +61,26 @@ DAEMON=$NAME
 SCRIPTNAME=/etc/init.d/$NAME
 
 checkcnfg() {
-    exists=0
-    for i in `echo "$PATH" | tr ':' '\n'` ; do
-        if [ -f $i/$NAME ] ; then
-            exists=1
-            break
-        fi
-    done
-    if [ $exists -eq 0 ] ; then
-        echo "cannot find rtorrent binary in PATH $PATH" | tee -a "$logfile" >&2
-        exit 3
+  exists=0
+  for i in `echo "$PATH" | tr ':' '\n'` ; do
+    if [ -f $i/$NAME ] ; then
+      exists=1
+      break
     fi
-    if ! [ -r "${config}" ] ; then 
-        echo "cannot find readable config ${config}. check that it is there and permissions are appropriate" | tee -a "$logfile" >&2
-        exit 3 
-    fi 
-    session=`getsession "$config"` 
-    if ! [ -d "${session}" ] ; then
-        echo "cannot find readable session directory ${session} from config ${config}. check permissions" | tee -a "$logfile" >&2
-        exit 3
-    fi
+  done
+  if [ $exists -eq 0 ] ; then
+    echo "cannot find rtorrent binary in PATH: $PATH" | tee -a "$logfile" >&2
+    exit 3
+  fi
+  if ! [ -r "${config}" ] ; then
+    echo "cannot find readable config ${config}. check that it is there and permissions are appropriate" | tee -a "$logfile" >&2
+    exit 3
+  fi
+  session=`getsession "$config"`
+  if ! [ -d "${session}" ] ; then
+    echo "cannot find readable session directory ${session} from config ${config}. check permissions" | tee -a "$logfile" >&2
+    exit 3
+  fi
 }
 
 d_start() {
@@ -93,19 +94,19 @@ d_start() {
 }
 
 d_stop() {
-    session=`getsession "$config"`
-    if ! [ -s ${session}/rtorrent.lock ] ; then
-        return
-    fi
-    pid=`cat ${session}/rtorrent.lock | awk -F: '{print($2)}' | sed "s/[^0-9]//g"`
-    if ps -A | grep -sq ${pid}.*rtorrent ; then # make sure the pid doesn't belong to another process
-        kill -s INT ${pid}
-    fi
+  session=`getsession "$config"`
+  if ! [ -s ${session}/rtorrent.lock ] ; then
+    return
+  fi
+  pid=`cat ${session}/rtorrent.lock | awk -F: '{print($2)}' | sed "s/[^0-9]//g"`
+  if ps -A | grep -sq ${pid}.*rtorrent ; then # make sure the pid doesn't belong to another process
+    kill -s INT ${pid}
+  fi
 }
 
-getsession() { 
-    session=`cat "$1" | grep "^[[:space:]]*session.path.set[[:space:]]*=" | sed "s/^[[:space:]]*session.path.set[[:space:]]*=[[:space:]]*//" `
-    echo $session
+getsession() {
+  session=`cat "$1" | grep "^[[:space:]]*session.path.set[[:space:]]*=" | sed "s/^[[:space:]]*session.path.set[[:space:]]*=[[:space:]]*//" `
+  echo $session
 }
 
 checkcnfg
