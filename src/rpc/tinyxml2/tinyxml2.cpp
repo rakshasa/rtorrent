185fccb1bab1bfb447ff60bc82fe591716367830
kannibalox <kannibalox@gmail.com>
2024-11-01 08:52:41 -0400

    Change rpc namespace to include static consts

--- src/rpc/tinyxml2/tinyxml2.cpp
+++ src/rpc/tinyxml2/tinyxml2.cpp
@@ -35,6 +35,8 @@ distribution.
 #   include <cstdarg>
 #endif
 
+namespace rpc { // rpc is a rtorrent-specific namespace
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1400 ) && (!defined WINCE)
 	// Microsoft Visual Studio, version 2005 and higher. Not WinCE.
 	/*int _snprintf_s(
@@ -133,7 +135,7 @@ static const unsigned char TIXML_UTF_LEAD_0 = 0xefU;
 static const unsigned char TIXML_UTF_LEAD_1 = 0xbbU;
 static const unsigned char TIXML_UTF_LEAD_2 = 0xbfU;
 
-namespace rpc::tinyxml2 // rpc is a rtorrent-specific namespace
+namespace tinyxml2
 {
 
 struct Entity {
@@ -3027,5 +3029,7 @@ bool XMLPrinter::Visit( const XMLUnknown& unknown )
     return true;
 }
 
-}   // namespace rpc::tinyxml2
+}   // namespace tinyxml2
+
+}   // namespace rpc
 #endif
