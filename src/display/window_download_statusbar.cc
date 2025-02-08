a8b2c0aac5b786d19a801d8b0d6884545964dc78
rakshasa <sundell.software@gmail.com>
2025-02-08 18:12:09 +0100

    Fixed threaded tracker feature compatibility.

--- src/display/window_download_statusbar.cc
+++ src/display/window_download_statusbar.cc
@@ -58,7 +58,7 @@ WindowDownloadStatusbar::redraw() {
   print_download_status(buffer, last, m_download);
   m_canvas->print(0, 2, "[%c:%i] %s",
                   m_download->tracker_list()->has_active() ? 'C' : ' ',
-                  (int)(m_download->download()->tracker_controller()->seconds_to_next_timeout()),
+                  (int)(m_download->download()->tracker_controller().seconds_to_next_timeout()),
                   buffer);
 }
 
