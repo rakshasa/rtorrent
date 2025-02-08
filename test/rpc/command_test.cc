89b35af7a6ce157eda7bc5bff20cb3726ca59a80
rakshasa <sundell.software@gmail.com>
2025-02-08 18:12:09 +0100

    Fixed threaded tracker feature compatibility.

--- test/rpc/command_test.cc
+++ test/rpc/command_test.cc
@@ -6,8 +6,8 @@ CPPUNIT_TEST_SUITE_REGISTRATION(CommandTest);
 
 bool
 command_stack_all_empty() {
-  return std::find_if(rpc::command_base::stack_begin(), rpc::command_base::stack_end(),
-                      std::mem_fun_ref(&torrent::Object::is_not_empty)) == rpc::command_base::stack_end();
+  return std::all_of(rpc::command_base::stack_begin(), rpc::command_base::stack_end(),
+                     [](auto& obj) { return obj.is_empty(); });
 }
 
 void
@@ -31,7 +31,7 @@ CommandTest::test_stack() {
   last_stack = rpc::command_base::push_stack(args, &stack);
   CPPUNIT_ASSERT(!command_stack_all_empty());
   CPPUNIT_ASSERT(rpc::command_base::stack_begin()->as_value() == 1);
-  
+
   rpc::command_base::pop_stack(&stack, last_stack);
   CPPUNIT_ASSERT(command_stack_all_empty());
 
@@ -44,7 +44,7 @@ CommandTest::test_stack() {
   CPPUNIT_ASSERT(!command_stack_all_empty());
   CPPUNIT_ASSERT(rpc::command_base::current_stack[0].as_value() == 2);
   CPPUNIT_ASSERT(rpc::command_base::current_stack[1].as_value() == 3);
-  
+
   rpc::command_base::pop_stack(&stack, last_stack);
   CPPUNIT_ASSERT(command_stack_all_empty());
 }
@@ -59,7 +59,7 @@ CommandTest::test_stack_double() {
 
   // Test double-stacked.
   args.push_back(int64_t(1));
-  
+
   last_stack_first = rpc::command_base::push_stack(args, &stack_first);
   CPPUNIT_ASSERT(!command_stack_all_empty());
   CPPUNIT_ASSERT(rpc::command_base::current_stack[0].as_value() == 1);
@@ -67,17 +67,17 @@ CommandTest::test_stack_double() {
   args.clear();
   args.push_back(int64_t(2));
   args.push_back(int64_t(3));
-  
+
   last_stack_second = rpc::command_base::push_stack(args, &stack_second);
   CPPUNIT_ASSERT(!command_stack_all_empty());
-  
+
   CPPUNIT_ASSERT(rpc::command_base::current_stack[0].as_value() == 2);
   CPPUNIT_ASSERT(rpc::command_base::current_stack[1].as_value() == 3);
 
   rpc::command_base::pop_stack(&stack_second, last_stack_second);
   CPPUNIT_ASSERT(!command_stack_all_empty());
   CPPUNIT_ASSERT(rpc::command_base::current_stack[0].as_value() == 1);
-  
+
   rpc::command_base::pop_stack(&stack_first, last_stack_first);
   CPPUNIT_ASSERT(command_stack_all_empty());
 }
