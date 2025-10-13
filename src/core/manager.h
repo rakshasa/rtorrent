891a6ba69dda39ad69593908cc1cf3341e03ddb5
Jari Sundell <sundell.software@gmail.com>
2025-10-13 12:09:20 +0200

    Added local address commands for inet/inet6.

--- src/core/manager.h
+++ src/core/manager.h
@@ -61,10 +61,6 @@ public:
 
   void                listen_open();
 
-  void                set_bind_address(const std::string& addr);
-  void                set_bind_inet_address(const std::string& addr);
-  void                set_bind_inet6_address(const std::string& addr);
-  void                set_local_address(const std::string& addr);
   void                set_proxy_address(const std::string& addr);
 
   void                shutdown(bool force);
