854c49274402b24a846a16075d11d268aab3ec63
rakshasa <sundell.software@gmail.com>
2026-01-12 15:22:57 +0100

    Stuff.

--- src/command_peer.cc
+++ src/command_peer.cc
@@ -1,33 +1,18 @@
 #include "config.h"
 
-#include <rak/string_manip.h>
 #include <torrent/bitfield.h>
 #include <torrent/rate.h>
 #include <torrent/net/socket_address.h>
 #include <torrent/peer/connection_list.h>
 #include <torrent/peer/peer.h>
 #include <torrent/peer/peer_info.h>
-
-#include "core/manager.h"
-#include "display/utils.h"
+#include <torrent/utils/string_manip.h>
 
 #include "globals.h"
 #include "control.h"
 #include "command_helpers.h"
-
-torrent::Object
-retrieve_p_id(torrent::Peer* peer) {
-  const torrent::HashString* hashString = &peer->id();
-
-  return rak::transform_hex(hashString->begin(), hashString->end());
-}
-
-torrent::Object
-retrieve_p_id_html(torrent::Peer* peer) {
-  const torrent::HashString* hashString = &peer->id();
-
-  return rak::copy_escape_html(hashString->begin(), hashString->end());
-}
+#include "core/manager.h"
+#include "display/utils.h"
 
 torrent::Object
 retrieve_p_address(torrent::Peer* peer) {
@@ -41,21 +26,11 @@ retrieve_p_address(torrent::Peer* peer) {
 }
 
 torrent::Object
-retrieve_p_port(torrent::Peer* peer) {
-  return torrent::sa_port(peer->peer_info()->socket_address());
-}
-
-torrent::Object
 retrieve_p_client_version(torrent::Peer* peer) {
   char buf[128];
   display::print_client_version(buf, buf + 128, peer->peer_info()->client_info());
 
   return std::string(buf);
-}
-
-torrent::Object
-retrieve_p_options_str(torrent::Peer* peer) {
-  return rak::transform_hex(peer->peer_info()->options(), peer->peer_info()->options() + 8);
 }
 
 torrent::Object
@@ -65,37 +40,37 @@ retrieve_p_completed_percent(torrent::Peer* peer) {
 
 void
 initialize_command_peer() {
-  CMD2_PEER("p.id",                std::bind(&retrieve_p_id, std::placeholders::_1));
-  CMD2_PEER("p.id_html",           std::bind(&retrieve_p_id_html, std::placeholders::_1));
-  CMD2_PEER("p.client_version",    std::bind(&retrieve_p_client_version, std::placeholders::_1));
+  CMD2_PEER("p.id",                [](auto* peer, auto) { return torrent::hash_string_to_hex_str(peer->id()); });
+  CMD2_PEER("p.id_html",           [](auto* peer, auto) { return torrent::hash_string_to_html_str(peer->id()); });
+  CMD2_PEER("p.client_version",    [](auto* peer, auto) { return retrieve_p_client_version(peer); });
 
-  CMD2_PEER("p.options_str",       std::bind(&retrieve_p_options_str, std::placeholders::_1));
+  CMD2_PEER("p.options_str",       [](auto* peer, auto) { return torrent::utils::string_from_hex_or_empty(peer->peer_info()->options(), peer->peer_info()->options() + 8); });
 
-  CMD2_PEER("p.is_encrypted",      std::bind(&torrent::Peer::is_encrypted, std::placeholders::_1));
-  CMD2_PEER("p.is_incoming",       std::bind(&torrent::Peer::is_incoming, std::placeholders::_1));
-  CMD2_PEER("p.is_obfuscated",     std::bind(&torrent::Peer::is_obfuscated, std::placeholders::_1));
-  CMD2_PEER("p.is_snubbed",        std::bind(&torrent::Peer::is_snubbed, std::placeholders::_1));
+  CMD2_PEER("p.is_encrypted",      [](auto* peer, auto) { return peer->is_encrypted(); });
+  CMD2_PEER("p.is_incoming",       [](auto* peer, auto) { return peer->is_incoming(); });
+  CMD2_PEER("p.is_obfuscated",     [](auto* peer, auto) { return peer->is_obfuscated(); });
+  CMD2_PEER("p.is_snubbed",        [](auto* peer, auto) { return peer->is_snubbed(); });
 
-  CMD2_PEER("p.is_unwanted",       std::bind(&torrent::PeerInfo::is_unwanted,  std::bind(&torrent::Peer::peer_info, std::placeholders::_1)));
-  CMD2_PEER("p.is_preferred",      std::bind(&torrent::PeerInfo::is_preferred, std::bind(&torrent::Peer::peer_info, std::placeholders::_1)));
+  CMD2_PEER("p.is_unwanted",       [](auto* peer, auto) { return peer->peer_info()->is_unwanted(); });
+  CMD2_PEER("p.is_preferred",      [](auto* peer, auto) { return peer->peer_info()->is_preferred(); });
 
-  CMD2_PEER("p.address",           std::bind(&retrieve_p_address, std::placeholders::_1));
-  CMD2_PEER("p.port",              std::bind(&retrieve_p_port, std::placeholders::_1));
+  CMD2_PEER("p.address",           [](auto* peer, auto) { return retrieve_p_address(peer); });
+  CMD2_PEER("p.port",              [](auto* peer, auto) { return torrent::sa_port(peer->peer_info()->socket_address()); });
 
-  CMD2_PEER("p.completed_percent", std::bind(&retrieve_p_completed_percent, std::placeholders::_1));
+  CMD2_PEER("p.completed_percent", [](auto* peer, auto) { return retrieve_p_completed_percent(peer); });
 
-  CMD2_PEER("p.up_rate",           std::bind(&torrent::Rate::rate,  std::bind(&torrent::Peer::up_rate, std::placeholders::_1)));
-  CMD2_PEER("p.up_total",          std::bind(&torrent::Rate::total, std::bind(&torrent::Peer::up_rate, std::placeholders::_1)));
-  CMD2_PEER("p.down_rate",         std::bind(&torrent::Rate::rate,  std::bind(&torrent::Peer::down_rate, std::placeholders::_1)));
-  CMD2_PEER("p.down_total",        std::bind(&torrent::Rate::total, std::bind(&torrent::Peer::down_rate, std::placeholders::_1)));
-  CMD2_PEER("p.peer_rate",         std::bind(&torrent::Rate::rate,  std::bind(&torrent::Peer::peer_rate, std::placeholders::_1)));
-  CMD2_PEER("p.peer_total",        std::bind(&torrent::Rate::total, std::bind(&torrent::Peer::peer_rate, std::placeholders::_1)));
+  CMD2_PEER("p.up_rate",           [](auto* peer, auto) { return peer->up_rate()->rate(); });
+  CMD2_PEER("p.up_total",          [](auto* peer, auto) { return peer->up_rate()->total(); });
+  CMD2_PEER("p.down_rate",         [](auto* peer, auto) { return peer->down_rate()->rate(); });
+  CMD2_PEER("p.down_total",        [](auto* peer, auto) { return peer->down_rate()->total(); });
+  CMD2_PEER("p.peer_rate",         [](auto* peer, auto) { return peer->peer_rate()->rate(); });
+  CMD2_PEER("p.peer_total",        [](auto* peer, auto) { return peer->peer_rate()->total(); });
 
-  CMD2_PEER        ("p.snubbed",     std::bind(&torrent::Peer::is_snubbed,  std::placeholders::_1));
-  CMD2_PEER_VALUE_V("p.snubbed.set", std::bind(&torrent::Peer::set_snubbed, std::placeholders::_1, std::placeholders::_2));
-  CMD2_PEER        ("p.banned",      std::bind(&torrent::Peer::is_banned,   std::placeholders::_1));
-  CMD2_PEER_VALUE_V("p.banned.set",  std::bind(&torrent::Peer::set_banned,  std::placeholders::_1, std::placeholders::_2));
+  CMD2_PEER        ("p.snubbed",     [](auto* peer, auto)       { return peer->is_snubbed(); });
+  CMD2_PEER_VALUE_V("p.snubbed.set", [](auto* peer, auto value) { peer->set_snubbed(value); });
+  CMD2_PEER        ("p.banned",      [](auto* peer, auto)       { return peer->is_banned(); });
+  CMD2_PEER_VALUE_V("p.banned.set",  [](auto* peer, auto value) { peer->set_banned(value); });
 
-  CMD2_PEER_V("p.disconnect",         std::bind(&torrent::Peer::disconnect, std::placeholders::_1, 0));
-  CMD2_PEER_V("p.disconnect_delayed", std::bind(&torrent::Peer::disconnect, std::placeholders::_1, torrent::ConnectionList::disconnect_delayed));
+  CMD2_PEER_V("p.disconnect",         [](auto* peer, auto) { peer->disconnect(0); });
+  CMD2_PEER_V("p.disconnect_delayed", [](auto* peer, auto) { peer->disconnect(torrent::ConnectionList::disconnect_delayed); });
 }
