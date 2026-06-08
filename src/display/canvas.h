f34c17b6c61981f21c1615c4675f49007798a46e
fffe <fffe@users.noreply.github.com>
2026-06-08 16:59:47 +0000

    set maybe_unused for both, everywhere

--- src/display/canvas.h
+++ src/display/canvas.h
@@ -132,8 +132,7 @@ Canvas::resize_term(std::pair<int, int> dim) {
 
 inline unsigned int
 Canvas::get_x() {
-  int x;
-  [[maybe_unused]] int y;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getyx(m_window, y, x);
   } else {
@@ -144,8 +143,7 @@ Canvas::get_x() {
 
 inline unsigned int
 Canvas::get_y() {
-  int y;
-  [[maybe_unused]] int x;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getyx(m_window, y, x);
   } else {
@@ -156,8 +154,7 @@ Canvas::get_y() {
 
 inline unsigned int
 Canvas::width() {
-  int x;
-  [[maybe_unused]] int y;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getmaxyx(m_window, y, x);
   } else {
@@ -168,8 +165,7 @@ Canvas::width() {
 
 inline unsigned int
 Canvas::height() {
-  int y;
-  [[maybe_unused]] int x;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getmaxyx(m_window, y, x);
   } else {
@@ -259,8 +255,7 @@ Canvas::set_default_attributes(int attr) {
 
 inline int
 Canvas::get_screen_width() {
-  int x;
-  [[maybe_unused]] int y;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getmaxyx(stdscr, y, x);
   } else {
@@ -271,8 +266,7 @@ Canvas::get_screen_width() {
 
 inline int
 Canvas::get_screen_height() {
-  int y;
-  [[maybe_unused]] int x;
+  [[maybe_unused]] int x, y;
   if (!m_daemon) {
     getmaxyx(stdscr, y, x);
   } else {
