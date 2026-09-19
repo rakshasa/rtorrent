9ab701bb5d7a08bf1a76197a6363e7efc5415618
xirvik <community@xirvik.net>
2026-09-19 02:11:20 +0000

    Bound JSON-RPC input by size and nesting depth
    Scan nesting before parsing and apply network.xmlrpc.size_limit to the JSON path too.

--- src/rpc/rpc_manager.cc
+++ src/rpc/rpc_manager.cc
@@ -173,6 +173,7 @@ RpcManager::set_size_limit(uint64_t size) {
     throw torrent::input_error("XMLRPC size limit cannot exceed the SCGI content size limit.");
 
   m_xmlrpc.set_size_limit(size);
+  m_jsonrpc.set_size_limit(size);
 }
 
 void
